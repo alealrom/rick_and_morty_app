eda439bc61cde1355ee8f5b25f5a3ab191ede449
alealrom <alealrom@gmail.com>
2022-11-28 14:30:38 -0500

    Header component has been updated in logo.

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 function Header({ brand }) {
   return (
     <header class='header'>
-        <img class='header__logo' href='/' src={ logo }></img>
+        <a href='/'><img class='header__logo' src={ logo } alt='Logo in the form of a drawing the animated series Rick and Morty, does not follow grammatical conventions, the characters have different sizes and seem to hang in space, the inner part of the title is painted in blue and the outer part is surrounded by green.'></img></a>
         <h1 class='header__title'>{ brand }</h1>
     </header>
   );
